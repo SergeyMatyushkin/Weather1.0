ed50b97c98bb30ea20d378314c84a7ab47239d0f
Sergey <gecktor87@gmail.com>
2021-07-25 17:15:09 +0300

    Подключен сервис.

--- app/src/main/java/com/example/weather10/view/MainActivity.kt
+++ app/src/main/java/com/example/weather10/view/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.weather10.view
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weather10.R
@@ -8,7 +10,8 @@ import com.example.weather10.view.main.MainFragment
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:  MainActivityBinding
+    private lateinit var binding: MainActivityBinding
+    private val receiver = MainBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,15 @@ class MainActivity : AppCompatActivity() {
                 .commitAllowingStateLoss()
         }
 
+        //регистрируем MainBroadcastReceiver
+        //подписываемся на сообщение перехода в режим самолета
+        registerReceiver(receiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
     }
 
+    override fun onDestroy() {
+        //отписываемся от сообщения перехода в режим самолета
+        unregisterReceiver(receiver)
+        super.onDestroy()
+
+    }
 }
