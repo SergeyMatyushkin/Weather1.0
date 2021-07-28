76147a6931b93d4d78f9e4462bd95d76e40ef123
Sergey <gecktor87@gmail.com>
2021-07-28 21:34:24 +0300

    Подключена библиотека OkHttp

--- app/src/main/java/com/example/weather10/view/details/DetailsFragment.kt
+++ app/src/main/java/com/example/weather10/view/details/DetailsFragment.kt
@@ -1,6 +1,11 @@
 package com.example.weather10.view.details
 
 import android.os.Build
+import android.os.Handler
+import com.example.weather10.BuildConfig
+import com.google.gson.Gson
+import okhttp3.*
+import java.io.IOException
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -12,31 +17,17 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.weather10.R
 import com.example.weather10.databinding.FragmentDetailsBinding
-import com.example.weather10.model.FactDTO
 import com.example.weather10.model.Weather
 import com.example.weather10.model.WeatherDTO
 
 
-//переменные для создания собственного интент-фильтра и
-// передачи данных
-const val DETAILS_INTENT_FILTER = "DETAILS INTENT FILTER"
-const val DETAILS_LOAD_RESULT_EXTRA = "LOAD RESULT"
-const val DETAILS_INTENT_EMPTY_EXTRA = "INTENT IS EMPTY"
-const val DETAILS_DATA_EMPTY_EXTRA = "DATA IS EMPTY"
-const val DETAILS_RESPONSE_EMPTY_EXTRA = "RESPONSE IS EMPTY"
-const val DETAILS_REQUEST_ERROR_EXTRA = "REQUEST ERROR"
-const val DETAILS_REQUEST_ERROR_MESSAGE_EXTRA = "REQUEST ERROR MESSAGE"
-const val DETAILS_URL_MALFORMED_EXTRA = "URL MALFORMED"
-const val DETAILS_RESPONSE_SUCCESS_EXTRA = "RESPONSE SUCCESS"
-const val DETAILS_TEMP_EXTRA = "TEMPERATURE"
-const val DETAILS_FEELS_LIKE_EXTRA = "FEELS LIKE"
-const val DETAILS_CONDITION_EXTRA = "CONDITION"
-private const val TEMP_INVALID = -100
-private const val FEELS_LIKE_INVALID = -100
+
 private const val PROCESS_ERROR = "Обработка ошибки"
+private const val REQUEST_API_KEY = "X-Yandex-API-Key"
+private const val MAIN_LINK = "https://api.weather.yandex.ru/v2/informers?"
+private const val WEATHER_API_KEY = "8723e083-1328-4c61-8c0d-46dbd00e11fd"
 
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
@@ -44,50 +35,7 @@ class DetailsFragment : Fragment() {
 
     private lateinit var weatherBundle: Weather
 
-    //Создаём свой BroadcastReceiver (получатель широковещательного сообщения)
-    private val loadResultsReceiver: BroadcastReceiver = object :
-        BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            //Достаём данные из интента
-            //В интенте бродкаста всегда будет отправляться флаг,указывающий на результат работы сервиса:
-            //через when мы сможем прочитать и обработать результат
-            when (intent.getStringExtra(DETAILS_LOAD_RESULT_EXTRA)) {
-                DETAILS_INTENT_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
-                DETAILS_DATA_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
-                DETAILS_RESPONSE_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
-                DETAILS_REQUEST_ERROR_EXTRA -> TODO(PROCESS_ERROR)
-                DETAILS_REQUEST_ERROR_MESSAGE_EXTRA -> TODO(PROCESS_ERROR)
-                DETAILS_URL_MALFORMED_EXTRA -> TODO(PROCESS_ERROR)
-                DETAILS_RESPONSE_SUCCESS_EXTRA -> renderData(
-                    WeatherDTO(
-                        FactDTO(
-                            intent.getIntExtra(
-                                DETAILS_TEMP_EXTRA, TEMP_INVALID
-                            ),
-                            intent.getIntExtra(
-                                DETAILS_FEELS_LIKE_EXTRA,
-                                FEELS_LIKE_INVALID
-                            ),
-                            intent.getStringExtra(
-                                DETAILS_CONDITION_EXTRA
-                            )
-                        )
-                    )
-                )
-                else -> TODO(PROCESS_ERROR)
-            }
-        }
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //подписываемся на BroadcastReceiver
-        //LocalBroadcastManager более эффективен в пересылке сообщений внутри приложения
-        context?.let {
-            LocalBroadcastManager.getInstance(it)
-                .registerReceiver(loadResultsReceiver, IntentFilter(DETAILS_INTENT_FILTER))
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -104,30 +52,51 @@ class DetailsFragment : Fragment() {
         weatherBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Weather()
         getWeather()
     }
-    //получаем данные
+    //запрашиваем данные у сервера
     private fun getWeather() {
         binding.viewDetailsFragment.visibility = View.GONE
         binding.loadingLayout.visibility = View.VISIBLE
-        context?.let {
-            //запускаем сервис
-            it.startService(Intent(it, DetailsService::class.java).apply {
-                putExtra(LATITUDE_EXTRA, weatherBundle.city.lat)
-                putExtra(LONGITUDE_EXTRA, weatherBundle.city.lon)
-            })
-        }
+        val client = OkHttpClient() // Клиент
+        val builder: Request.Builder = Request.Builder() // Создаём строителя запроса
+        builder.header(REQUEST_API_KEY, WEATHER_API_KEY) // Создаём заголовок запроса
+        // Формируем URL
+        builder.url(MAIN_LINK + "lat=${weatherBundle.city.lat}&lon=${weatherBundle.city.lon}")
+        val request: Request = builder.build() // Создаём запрос
+        val call: Call = client.newCall(request)
+        // Ставим запрос в очередь и отправляем
+        call.enqueue(object : Callback {
+            val handler: Handler = Handler()
+
+            // Вызывается, если ответ от сервера пришёл
+            @Throws(IOException::class)
+            override fun onResponse(call: Call?, response: Response) {
+                val serverResponse: String? = response.body()?.string()
+                // Синхронизируем поток с потоком UI
+                if (response.isSuccessful && serverResponse != null) {
+                    handler.post {
+                        renderData(Gson().fromJson(serverResponse, WeatherDTO::class.java))
+                    }
+                } else {
+                    TODO(PROCESS_ERROR)
+                }
+            }
+
+            // Вызывается при сбое в процессе запроса на сервер
+            override fun onFailure(call: Call?, e: IOException?) {
+                TODO(PROCESS_ERROR)
+            }
+        })
     }
 
     //отображаем данные
     private fun renderData(weatherDTO: WeatherDTO) {
         binding.viewDetailsFragment.visibility = View.VISIBLE
         binding.loadingLayout.visibility = View.GONE
-
         val fact = weatherDTO.fact
-        val temp = fact!!.temp
-        val feelsLike = fact.feels_like
-        val condition = fact.condition
-        if (temp == TEMP_INVALID || feelsLike == FEELS_LIKE_INVALID || condition == null) {
-            TODO("Обработка ошибки")
+        if (fact == null || fact.temp == null || fact.feels_like == null ||
+            fact.condition.isNullOrEmpty()
+        ) {
+            TODO(PROCESS_ERROR)
         } else {
             val city = weatherBundle.city
             binding.cityName.text = city.city
@@ -136,9 +105,9 @@ class DetailsFragment : Fragment() {
                 city.lat.toString(),
                 city.lon.toString()
             )
-            binding.weatherCondition.text = condition
-            binding.temperatureValue.text = temp.toString()
-            binding.feelsLikeValue.text = feelsLike.toString()
+            binding.weatherCondition.text = fact.condition
+            binding.temperatureValue.text = fact.temp.toString()
+            binding.feelsLikeValue.text = fact.feels_like.toString()
         }
     }
 
@@ -147,13 +116,7 @@ class DetailsFragment : Fragment() {
         _binding = null
     }
 
-    override fun onDestroy() {
-        //отписываемся от BroadcastReceiver  c пом. LocalBroadcastManager
-        context?.let {
-            LocalBroadcastManager.getInstance(it).unregisterReceiver(loadResultsReceiver)
-        }
-        super.onDestroy()
-    }
+
 
     companion object {
         //наш ключ-константа,по которому будем находить бандл
